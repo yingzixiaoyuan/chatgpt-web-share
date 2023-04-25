533f86a7380e18c55d547b92767674039b9b5183
chulihui <chulihui@nibirutech.com>
2023-04-25 22:40:00 +0800

    feat: 删除百度

--- frontend/src/components/PageHeader.vue
+++ frontend/src/components/PageHeader.vue
@@ -196,12 +196,4 @@ const jumpToAdminOrConv = async () => {
     await router.push({ name: 'admin' });
   }
 }
-
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?4b7823b96cb7936e075b543d7b9f01ec";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
 </script>
